b62f08497df2a8ebb79a10da0f8a6f1e594c7cc6
Sarah Vitoria <sarahvitoriaffranca4@gmail.com>
2025-08-27 17:52:32 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,12 +1,15 @@
-var pesoE = Number(prompt("Qual o peso?"))
-var multa = pesoE - 50
+// function tabuada(){
+//     var num = parseInt(document.getElementById("num").value);
+//     var resposta = document.getElementById("resposta");
+//     var count;
 
-if ( pesoE >= 50 ) {
-    alert(`Passou ${multa}kg do peso.`)
-    multa = Number(multa*4)
-    alert(`O valor da multa é ${multa} reais.`)
-} else {
-    alert(`
-        Não passou do peso.
-        Multa Igual a 0.`)
+//     for(count=1; count<=10; count++){
+//         count*8
+//     }
+// }
+
+function tabuada(){
+    for(count=1; count<=10; count++){
+        console.log(count*8)
+    }
 }
