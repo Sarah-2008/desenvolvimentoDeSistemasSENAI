9ecd88914dc654057322b5227d990a2a0c73cd81
sarah <sarahvitoriaffranca4@gmail.com>
2025-12-04 16:36:22 -0300

    Aula de banco de dados

---
+++ Banco de dados - Básico/aula 3/material_2.sql
@@ -0,0 +1,18 @@
+USE Netflix;
+
+# CRIAR TABELA
+CREATE TABLE IF NOT EXISTS filmes (
+	id INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(50) UNIQUE NOT NULL,
+	# genero VARCHAR(50) NOT NULL,
+    genero ENUM("TERROR", "AÇÃO", "FICÇÃO"),
+    diretor VARCHAR(30),
+    estudio VARCHAR(50) NOT NULL DEFAULT 'Netflix',
+    PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8;
+
+# ALTER TABLE filmes ADD duracao TIME;
+# ALTER TIME filmes DROP COLUMN duracao;
+ALTER TABLE filmes MODIFY COLUMN diretor VARCHAR(100);
+
+SELECT * FROM filmes;
